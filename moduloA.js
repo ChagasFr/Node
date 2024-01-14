d88d40ca56e6bdac5dfea5c1116e0984ab8828b9
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-01-14 20:57:13 -0300

    modulos

---
+++ moduloA.js
@@ -0,0 +1,4 @@
+// atributos dinamicos
+this.ola = 'fala pessoal'
+exports.bemVindo = 'Bem vindo ao Node!'
+module.exports.ateLogo = "ate o proximo exemplo"
---
+++ moduloB.js
@@ -0,0 +1,7 @@
+// criando novos objs e assim disponiveis fora do modulo 
+module.exports = {
+    bomDia: 'Bom dia',
+    boaNoite() {
+        return 'Boa noite'
+    }
+}
