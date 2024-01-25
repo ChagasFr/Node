a61ee57ba35ef5cc65a8ff3e013007b0794d12c8
Felipe chagas <felipereis9998@aluno.saojudas.br>
2024-01-25 00:42:56 -0300

    feat: regras

--- temporizador.js
+++ temporizador.js
@@ -16,3 +16,7 @@ const regra = new schedule.RecurrenceRule()
 regra.dayOfWeek = [new schedule.Range(1, 5)]
 regra.hour = 12
 regra.second = 30
+
+const tarefa2 = schedule.scheduleJob(regra, function () {
+    console.log('Executando Tarefa 2!', new Date().getSeconds())
+})
